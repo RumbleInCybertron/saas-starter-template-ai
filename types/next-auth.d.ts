1e5fa2de8b679fdd74bb5d554a8fe3d42249885c
Derek <rumbleincybertron@gmail.com>
2025-07-13 23:04:17 +0900

    ch next auth

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,18 +1,19 @@
-import { User } from "next-auth"
-import { JWT } from "next-auth/jwt"
+import NextAuth from 'next-auth'
 
-type UserId = string
-
-declare module "next-auth/jwt" {
-  interface JWT {
-    id: UserId
-  }
-}
-
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session {
-    user: User & {
-      id: UserId
+    user: {
+      id: string
+      email: string
+      name?: string | null
+      image?: string | null
     }
+  }
+
+  interface User {
+    id: string
+    email: string
+    name?: string | null
+    image?: string | null
   }
 }
