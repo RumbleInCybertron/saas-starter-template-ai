f1f94d84f50d96f01b6fabaf6c75c581f168dda6
Derek <rumbleincybertron@gmail.com>
2025-07-13 23:04:47 +0900

    replace stripe library

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,8 +1,30 @@
-import Stripe from "stripe"
+import Stripe from 'stripe'
 
-import { env } from "@/env.mjs"
-
-export const stripe = new Stripe(env.STRIPE_API_KEY, {
-  apiVersion: "2022-11-15",
-  typescript: true,
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2024-06-20',
 })
+
+export const getStripeSession = async (
+  customerId: string,
+  priceId: string,
+  userId: string
+) => {
+  const session = await stripe.checkout.sessions.create({
+    customer: customerId,
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+    mode: 'subscription',
+    success_url: `${process.env.NEXTAUTH_URL}/dashboard?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${process.env.NEXTAUTH_URL}/pricing`,
+    metadata: {
+      userId,
+    },
+  })
+
+  return session
+}
