25319c5ed95d04824d879e56085e7e32b19d60fc
Derek <rumbleincybertron@gmail.com>
2025-07-14 23:48:28 +0900

    ch provider default theme to system

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,14 +1,15 @@
 'use client'
 
 import { SessionProvider } from 'next-auth/react'
-import { ThemeProvider } from 'next-themes'
+import { ThemeProvider } from '@/components/theme-provider'
+
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <ThemeProvider
         attribute="class"
-        defaultTheme="light"
+        defaultTheme="system"
         enableSystem
         disableTransitionOnChange
       >
